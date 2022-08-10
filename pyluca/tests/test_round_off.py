d23f904a5d54882f236436c9ba158cc71a8f392a
Pramod Kumar <pramodkumar.damam73@gmail.com>
2022-08-10 13:06:12 +0530

    Fix tests

--- pyluca/tests/test_round_off.py
+++ pyluca/tests/test_round_off.py
@@ -16,22 +16,19 @@ class TestRoundOff(TestCase):
         self.assertEqual(round_off_amount(1000.0001), 1000.0001)
         self.assertEqual(round_off_amount(1000.00001), 1000.00001)
         self.assertEqual(round_off_amount(1000.000001), 1000.000001)
-        self.assertEqual(round_off_amount(1000.0000001), 1000.0000001)
+        self.assertEqual(round_off_amount(1000.0000001), 1000.000001)
         self.assertNotEqual(round_off_amount(1000.00000001), 1000.00000001)
-        self.assertEqual(round_off_amount(1000.00000001), 1000.0000001)
-        self.assertEqual(round_off_amount(1000.00000019398493849898928), 1000.0000002)
-        self.assertEqual(round_off_amount(1000.0000001999999), 1000.0000002)
-        # following assertion is weird, because of python floating issue
-        self.assertEqual(round_off_amount(1000.00000019999999), 1000.0000003)
-        # weird end
+        self.assertEqual(round_off_amount(1000.00000001), 1000.000001)
+        self.assertEqual(round_off_amount(1000.0000019398493849898928), 1000.000002)
+        self.assertEqual(round_off_amount(1000.000001999999), 1000.000002)
+        self.assertEqual(round_off_amount(1000.000001999999999999999999999999), 1000.000002)
 
-        self.assertEqual(round_off_amount(1000.00000010001), 1000.0000002)
-        self.assertEqual(round_off_amount(1000.000000100001), 1000.0000002)
-        self.assertEqual(round_off_amount(1000.0000001000001), 1000.0000002)
+        self.assertEqual(round_off_amount(1000.0000010001), 1000.000002)
+        self.assertEqual(round_off_amount(1000.00000100001), 1000.000002)
+        self.assertEqual(round_off_amount(1000.000001000001), 1000.000002)
 
-        # following assertion is weird, because of python floating issue
-        self.assertEqual(round_off_amount(1000.00000010000001), 1000.0000001)
-        # weird end
+        # weired
+        self.assertEqual(round_off_amount(1000.000001000000000000000000001), 1000.000001)
 
     def test_zeroed(self):
         self.assertEqual(zeroed(0), 0)
@@ -45,18 +42,19 @@ class TestRoundOff(TestCase):
         self.assertEqual(zeroed(0.00000000001), 0)
         self.assertEqual(zeroed(0.000000001), 0)
         self.assertEqual(zeroed(0.0000001), 0)
-        self.assertEqual(zeroed(0.00001), 0)
+        self.assertEqual(zeroed(0.00001), 0.00001)
         self.assertEqual(zeroed(0.0001), 0.0001)
         self.assertEqual(zeroed(0.001), 0.001)
         self.assertEqual(zeroed(0.01), 0.01)
         self.assertEqual(zeroed(0.1), 0.1)
-        self.assertEqual(zeroed(-0.00001), 0)
+        self.assertEqual(zeroed(-0.00001), -0.00001)
+        self.assertEqual(zeroed(-0.000001), -0.000001)
 
     def test_with_amount_counter(self):
         ac = AmountCounter(100)
         ac.pay(99, datetime(2022, 8, 10))
         self.assertEqual(ac.get_balance(), 1)
-        p1, p2 = .99938838883, .0006
+        p1, p2 = .9999938838883, .000006
         self.assertNotEqual(p1 + p2, 0)
         ac.pay(p1, datetime(2022, 8, 10))
         self.assertFalse(ac.is_paid())
