c6dccc104cf8199e99bf1af1a758d3ab6bc1eb00
Pramod Kumar <pramodkumar.damam73@gmail.com>
2022-08-11 09:23:11 +0530

    Add test for high precision entries (#22)

--- pyluca/tests/test_aging.py
+++ pyluca/tests/test_aging.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 from pyluca.accountant import Accountant
 from pyluca.aging import get_account_aging
 from pyluca.journal import JournalEntry, Journal
+from pyluca.ledger import Ledger
 
 account_config = {
     'account_types': {
@@ -95,3 +96,39 @@ class TestAging(TestCase):
         due_meta = ages[0].meta['due_date'].split('-')
         due_date = datetime(int(due_meta[0]), int(due_meta[1]), int(due_meta[2]))
         self.assertEqual((ages[0].counter.payments[-1].date - due_date).days, 15)
+
+    def test_high_precision(self):
+        accountant = Accountant(Journal(), account_config, 'person1')
+        accountant.enter_journal('SAVINGS_BANK', 'SALARY', 200, datetime(2022, 8, 9), 'Salary')
+        accountant.enter_journal('LOANS', 'SAVINGS_BANK', 100.128839293829282838283823, datetime(2022, 8, 10), 'XYZ client')
+        accountant.enter_journal('LOANS_PAYBACK', 'LOANS', 100.12883, datetime(2022, 8, 10), 'XYZ client')
+        ages = get_account_aging(account_config, accountant.journal.entries, 'LOANS', datetime(2022, 8, 11))
+        age = ages[0]
+        self.assertTrue(age.counter.is_paid())
+        self.assertAlmostEqual(age.counter.get_balance(), 0, 4)
+
+        accountant = Accountant(Journal(), account_config, 'person1')
+        accountant.enter_journal('SAVINGS_BANK', 'SALARY', 200, datetime(2022, 8, 9), 'Salary')
+        accountant.enter_journal('LOANS', 'SAVINGS_BANK', 100.128839293829282838283823, datetime(2022, 8, 10),
+                                 'XYZ client')
+        accountant.enter_journal('LOANS_PAYBACK', 'LOANS', 100.1288, datetime(2022, 8, 10), 'XYZ client')
+        ages = get_account_aging(account_config, accountant.journal.entries, 'LOANS', datetime(2022, 8, 11))
+        age = ages[0]
+        self.assertFalse(age.counter.is_paid())
+
+        accountant = Accountant(Journal(), account_config, 'person1')
+        accountant.enter_journal('SAVINGS_BANK', 'SALARY', 200, datetime(2022, 8, 9), 'Salary')
+        accountant.enter_journal('LOANS', 'SAVINGS_BANK', 100.00000387278, datetime(2022, 8, 10),
+                                 'To a')
+        accountant.enter_journal('LOANS', 'SAVINGS_BANK', 200.00000387278, datetime(2022, 8, 10),
+                                 'To b')
+        accountant.enter_journal('LOANS_PAYBACK', 'LOANS', 100, datetime(2022, 8, 10), 'From a')
+        accountant.enter_journal('LOANS_PAYBACK', 'LOANS', 199.999994222222222, datetime(2022, 8, 10), 'From b')
+        ages = get_account_aging(account_config, accountant.journal.entries, 'LOANS', datetime(2022, 8, 11))
+        age1, age2 = ages[0], ages[1]
+        self.assertTrue(age1.counter.is_paid())
+        self.assertTrue(age2.counter.is_paid())
+        self.assertNotEqual(age1.counter.get_balance(), 0)
+        self.assertNotEqual(age2.counter.get_balance(), 0)
+        ledger = Ledger(accountant.journal, accountant.config)
+        self.assertAlmostEqual(ledger.get_account_balance('LOANS'), 0, 4)
