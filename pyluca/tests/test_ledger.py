1f9ecd5ecb838a4eaeb2774135d09f4de35fda3f
Ranjani R Rao <43045174+ranjanikrrao@users.noreply.github.com>
2022-09-22 20:29:56 +0530

    Add account_name column in ledger_df (#28)
    * Add account_name column in ledger_df
    
    * Added test cases

--- pyluca/tests/test_ledger.py
+++ pyluca/tests/test_ledger.py
@@ -46,3 +46,21 @@ class TestLedger(TestCase):
 
         self.assertEqual(15000, ledger.get_account_type_balance('ASSET', ['SAVINGS_BANK']))
         self.assertEqual(10000, ledger.get_account_type_balance('ASSET', ['SAVINGS_BANK', 'LOANS']))
+
+    def test_account_name_ledger_df(self):
+        accountant = Accountant(Journal(), account_config, '2')
+        accountant.enter_journal('SAVINGS_BANK', 'SALARY', 20000, datetime(2022, 4, 30), 'April salary')
+        accountant.enter_journal('MUTUAL_FUNDS', 'SAVINGS_BANK', 10000, datetime(2022, 5, 1), 'ELSS')
+        accountant.enter_journal('LOANS', 'SAVINGS_BANK', 5000, datetime(2022, 5, 2), 'Lend to Pramod')
+        accountant.enter_journal('CAR_EMI', 'SAVINGS_BANK', 3000, datetime(2022, 5, 2), 'EMI 3/48')
+        ledger = Ledger(accountant.journal, account_config)
+        ledger_df = ledger.get_df()
+        self.assertEqual(
+            'SAVINGS_ACCOUNT',
+            ledger_df[ledger_df['account'] == 'SAVINGS_BANK'].reset_index()._get_value(0, 'account_name')
+        )
+        self.assertEqual(
+            'SALARY_AMOUNT',
+            ledger_df[ledger_df['account'] == 'SALARY'].reset_index()._get_value(0, 'account_name')
+        )
+
