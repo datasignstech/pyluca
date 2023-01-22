903ef9f67811171ad6374c705450a9f276f9ed4a
Pramod <pramodkumar.damam73@gmail.com>
2023-01-22 18:56:00 +0530

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pyluca',
-    version='1.2.4',
+    version='1.3.0',
     author='datasignstech',
     author_email='tech+opensource@datasignstech.com',
     description='Double entry accounting system',
