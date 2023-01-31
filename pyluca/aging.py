26dc5304dc4a1f7f5dd8aa92f928a67cbc6a7c4d
Pramod Kumar <pramodkumar.damam73@gmail.com>
2023-01-31 22:49:48 +0530

    Remove PositiveEntry (#35)

--- pyluca/aging.py
+++ pyluca/aging.py
@@ -14,29 +14,23 @@ class AccountAge(NamedTuple):
     meta: Optional[dict]
 
 
-class PositiveEntry(NamedTuple):
-    date: datetime
-    counter: AmountCounter
-    meta: Optional[dict]
-
-
-class AgingState:
-    def __init__(self, account: str, positive_entries: List[PositiveEntry], excess_amount: float, sl_no: int):
+class AccountAging:
+    def __init__(self, account: str, ages: List[AccountAge], excess_amount: float, last_sl_no: int):
         self.account = account
-        self.positive_entries = positive_entries
+        self.ages = ages
         self.excess_amount = excess_amount
-        self.sl_no = sl_no
+        self.last_sl_no = last_sl_no
 
 
-def __pay_counters(positive_entries: List[PositiveEntry], amount: float, date: datetime) -> float:
-    if len(positive_entries) == 0:
+def __pay_counters(ages: List[AccountAge], amount: float, date: datetime) -> float:
+    if len(ages) == 0:
         return amount
     if amount == 0:
         return 0
     rem_amount, cur_idx = amount, 0
-    while rem_amount > 0 and cur_idx < len(positive_entries):
-        _, rem_amount = positive_entries[cur_idx].counter.pay(rem_amount, date)
-        if positive_entries[cur_idx].counter.is_paid():
+    while rem_amount > 0 and cur_idx < len(ages):
+        _, rem_amount = ages[cur_idx].counter.pay(rem_amount, date)
+        if ages[cur_idx].counter.is_paid():
             cur_idx += 1
         else:
             break
@@ -48,17 +42,18 @@ def get_account_aging(
         entries: List[JournalEntry],
         account: str,
         as_of: datetime,
-        previous_state: AgingState = None
-) -> Tuple[List[AccountAge], AgingState]:
+        previous_aging: AccountAging = None
+) -> AccountAging:
     def should_entry_applied(e: JournalEntry) -> bool:
-        return e.date <= as_of and e.account == account and (previous_state is None or e.sl_no > previous_state.sl_no)
+        return e.date <= as_of and e.account == account \
+               and (previous_aging is None or e.sl_no > previous_aging.last_sl_no)
 
-    state = deepcopy(previous_state)
-    if state is None:
-        state = AgingState(account, [], 0, -1)
+    aging = deepcopy(previous_aging)
+    if aging is None:
+        aging = AccountAging(account, [], 0, -1)
 
-    if state.account != account:
-        raise ValueError('Invalid previous state! account not matching')
+    if aging.account != account:
+        raise ValueError('Invalid previous aging! account not matching')
 
     account_type = config['accounts'][account]['type']
     for entry in entries:
@@ -69,13 +64,13 @@ def get_account_aging(
         negative_amount = entry.dr_amount if account_balance_type == BalanceType.CREDIT.value else entry.cr_amount
         if positive_amount > 0:
             meta = re.match('.*##(.*)##.*', entry.narration)
-            state.positive_entries.append(
-                PositiveEntry(
+            aging.ages.append(
+                AccountAge(
                     entry.date,
                     AmountCounter(positive_amount),
                     json.loads(meta.group(1)) if meta else None
                 )
             )
-        state.excess_amount = __pay_counters(state.positive_entries, state.excess_amount + negative_amount, entry.date)
-        state.sl_no = entry.sl_no
-    return [AccountAge(entry.date, entry.counter, entry.meta) for entry in state.positive_entries], state
+        aging.excess_amount = __pay_counters(aging.ages, aging.excess_amount + negative_amount, entry.date)
+        aging.last_sl_no = entry.sl_no
+    return aging
