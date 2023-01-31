afa54acd152f003b99979fe2e059cc734b98c77b
Pramod <pramodkumar.damam73@gmail.com>
2023-01-31 22:37:44 +0530

    Remove PositiveEntry

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pyluca',
-    version='2.0.0',
+    version='2.0.1',
     author='datasignstech',
     author_email='tech+opensource@datasignstech.com',
     description='Double entry accounting system',
