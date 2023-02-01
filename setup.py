7df6ad99d88ff91129d6eec630ba8d38076b1871
Pramod Kumar <pramodkumar.damam73@gmail.com>
2023-02-01 21:25:27 +0530

    Remove deepcopy from aging (#37)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pyluca',
-    version='2.0.2',
+    version='2.0.3',
     author='datasignstech',
     author_email='tech+opensource@datasignstech.com',
     description='Double entry accounting system',
