f0e31c450dba4dd87c4cb49fa9834a656813dbcc
sameer-639 <sameer.saxena@datasignsfintech.com>
2023-09-05 15:21:24 +0530

    Added get_accounts_aging function

--- pyluca/aging.py
+++ pyluca/aging.py
@@ -1,7 +1,7 @@
 import json
 import re
 from datetime import datetime
-from typing import NamedTuple, List, Optional
+from typing import NamedTuple, List, Optional, Dict, Tuple
 from pyluca.account_config import BalanceType
 from pyluca.journal import JournalEntry
 from pyluca.amount_counter import AmountCounter
@@ -15,26 +15,53 @@ class AccountAge(NamedTuple):
 
 
 class AccountAging:
-    def __init__(self, account: str, ages: List[AccountAge], excess_amount: float, last_sl_no: int):
+    def __init__(
+            self,
+            account: str,
+            ages: List[AccountAge],
+            excess_amount: float,
+            last_sl_no: int,
+            last_unpaid_age_idx: int = 0
+    ):
         self.account = account
         self.ages = ages
         self.excess_amount = excess_amount
         self.last_sl_no = last_sl_no
+        self.last_unpaid_age_idx = last_unpaid_age_idx
 
 
-def __pay_counters(ages: List[AccountAge], amount: float, date: datetime) -> float:
+def __pay_counters(ages: List[AccountAge], amount: float, date: datetime, last_unpaid_age_idx) -> Tuple[float, int]:
     if len(ages) == 0:
-        return amount
+        return amount, last_unpaid_age_idx
     if amount == 0:
-        return 0
-    rem_amount, cur_idx = amount, 0
-    while rem_amount > 0 and cur_idx < len(ages):
-        _, rem_amount = ages[cur_idx].counter.pay(rem_amount, date)
-        if ages[cur_idx].counter.is_paid():
-            cur_idx += 1
+        return 0, last_unpaid_age_idx
+    rem_amount = amount
+    while rem_amount > 0 and last_unpaid_age_idx < len(ages):
+        _, rem_amount = ages[last_unpaid_age_idx].counter.pay(rem_amount, date)
+        if ages[last_unpaid_age_idx].counter.is_paid():
+            last_unpaid_age_idx += 1
         else:
             break
-    return rem_amount
+    return rem_amount, last_unpaid_age_idx
+
+
+def __update_account_aging(account_balance_type: str, entry: JournalEntry, aging: AccountAging):
+    positive_amount = entry.cr_amount if account_balance_type == BalanceType.CREDIT.value else entry.dr_amount
+    negative_amount = entry.dr_amount if account_balance_type == BalanceType.CREDIT.value else entry.cr_amount
+    if positive_amount > 0:
+        meta = re.match('.*##(.*)##.*', entry.narration)
+        aging.ages.append(
+            AccountAge(
+                entry.date,
+                AmountCounter(positive_amount),
+                json.loads(meta.group(1)) if meta else None,
+                entry
+            )
+        )
+    aging.excess_amount, aging.last_unpaid_age_idx = __pay_counters(
+        aging.ages, aging.excess_amount + negative_amount, entry.date, aging.last_unpaid_age_idx
+    )
+    aging.last_sl_no = entry.sl_no
 
 
 def get_account_aging(
@@ -55,23 +82,38 @@ def get_account_aging(
     if aging.account != account:
         raise ValueError('Invalid previous aging! account not matching')
 
-    account_type = config['accounts'][account]['type']
-    account_balance_type = config['account_types'][account_type]['balance_type']
+    account_balance_type = config['account_types'][config['accounts'][account]['type']]['balance_type']
     for entry in entries:
         if not should_entry_applied(entry):
             continue
-        positive_amount = entry.cr_amount if account_balance_type == BalanceType.CREDIT.value else entry.dr_amount
-        negative_amount = entry.dr_amount if account_balance_type == BalanceType.CREDIT.value else entry.cr_amount
-        if positive_amount > 0:
-            meta = re.match('.*##(.*)##.*', entry.narration)
-            aging.ages.append(
-                AccountAge(
-                    entry.date,
-                    AmountCounter(positive_amount),
-                    json.loads(meta.group(1)) if meta else None,
-                    entry
-                )
-            )
-        aging.excess_amount = __pay_counters(aging.ages, aging.excess_amount + negative_amount, entry.date)
-        aging.last_sl_no = entry.sl_no
+        __update_account_aging(account_balance_type, entry, aging)
+    return aging
+
+
+def get_accounts_aging(
+        config: dict,
+        entries: List[JournalEntry],
+        accounts: List[str],
+        as_of: datetime,
+        previous_aging: Dict[str, AccountAging] = None
+) -> Dict[str, AccountAging]:
+    aging = previous_aging
+    if aging is None:
+        aging = {}
+        for account in accounts:
+            aging[account] = AccountAging(account, [], 0, -1)
+
+    if not all([aging.get(acc) for acc in accounts]):
+        raise ValueError('Invalid previous aging! accounts not matching')
+
+    def should_entry_applied(entry: JournalEntry) -> bool:
+        return entry.date <= as_of and aging.get(entry.account) is not None \
+               and (previous_aging is None or entry.sl_no > previous_aging[entry.account].last_sl_no)
+
+    entries = entries[max([aging.last_sl_no for aging in previous_aging.values()]) + 1:] if previous_aging else entries
+    for entry in entries:
+        if not should_entry_applied(entry):
+            continue
+        account_balance_type = config['account_types'][config['accounts'][entry.account]['type']]['balance_type']
+        __update_account_aging(account_balance_type, entry, aging[entry.account])
     return aging
--- pyluca/tests/test_aging.py
+++ pyluca/tests/test_aging.py
@@ -2,7 +2,7 @@ import json
 from datetime import datetime
 from unittest import TestCase
 from pyluca.accountant import Accountant
-from pyluca.aging import get_account_aging
+from pyluca.aging import get_account_aging, get_accounts_aging
 from pyluca.journal import JournalEntry, Journal
 from pyluca.ledger import Ledger
 
@@ -161,3 +161,99 @@ class TestAging(TestCase):
             raise AssertionError('Should not have passed because wrong previous state provided')
         except ValueError as e:
             self.assertEqual(str(e), 'Invalid previous aging! account not matching')
+
+    def test_get_accounts_aging(self):
+        dt = datetime.now()
+        aging = get_accounts_aging(account_config, [
+            JournalEntry(1, 'SAVINGS_BANK', 1000, 0, dt, '', '1', None),
+            JournalEntry(2, 'SAVINGS_BANK', 1000, 0, dt, '', '1', None),
+            JournalEntry(3, 'SAVINGS_BANK', 1000, 0, dt, '', '1', None),
+            JournalEntry(4, 'SAVINGS_BANK', 0, 4000, dt, '', '1', None),
+        ], ['SAVINGS_BANK'], dt)
+        for age in aging['SAVINGS_BANK'].ages:
+            self.assertEqual(age.counter.is_paid(), True)
+
+        aging = get_accounts_aging(account_config, [
+            JournalEntry(4, 'SAVINGS_BANK', 0, 4000, dt, '', '1', None),
+            JournalEntry(1, 'SAVINGS_BANK', 1000, 0, dt, '', '1', None),
+            JournalEntry(2, 'SAVINGS_BANK', 1000, 0, dt, '', '1', None),
+            JournalEntry(3, 'SAVINGS_BANK', 1000, 0, dt, '', '1', None),
+        ], ['SAVINGS_BANK'], dt)
+        for age in aging['SAVINGS_BANK'].ages:
+            self.assertEqual(age.counter.is_paid(), True)
+
+        aging = get_accounts_aging(account_config, [
+            JournalEntry(4, 'SAVINGS_BANK', 0, 3000, dt, '', '1', None),
+            JournalEntry(1, 'SAVINGS_BANK', 1000, 0, dt, '', '1', None),
+            JournalEntry(2, 'SAVINGS_BANK', 1000, 0, dt, '', '1', None),
+            JournalEntry(3, 'SAVINGS_BANK', 2000, 0, dt, '', '1', None),
+        ], ['SAVINGS_BANK'], dt)
+        ages = aging['SAVINGS_BANK'].ages
+        self.assertEqual(len(ages), 3)
+        self.assertEqual(ages[0].counter.is_paid(), True)
+        self.assertEqual(ages[1].counter.is_paid(), True)
+        self.assertEqual(ages[2].counter.is_paid(), False)
+        self.assertEqual(ages[2].counter.get_balance(), 1000)
+
+        aging = get_accounts_aging(account_config, [
+            JournalEntry(1, 'SAVINGS_BANK', 1000, 0, dt, '', '1', None),
+            JournalEntry(2, 'SAVINGS_BANK', 1000, 0, dt, '', '1', None),
+            JournalEntry(3, 'SAVINGS_BANK', 0, 3000, dt, '', '1', None),
+            JournalEntry(4, 'SAVINGS_BANK', 2000, 0, dt, '', '1', None),
+        ], ['SAVINGS_BANK'], dt)
+        ages = aging['SAVINGS_BANK'].ages
+        self.assertEqual(len(ages), 3)
+        self.assertEqual(ages[0].counter.is_paid(), True)
+        self.assertEqual(ages[1].counter.is_paid(), True)
+        self.assertEqual(ages[2].counter.is_paid(), False)
+        self.assertEqual(ages[2].counter.get_balance(), 1000)
+
+    def test_aging_state_with_get_accounts_aging(self):
+        accountant = Accountant(Journal(), account_config, 'person1')
+        accountant.enter_journal('SAVINGS_BANK', 'SALARY', 200, datetime(2022, 8, 9), 'Salary')
+        accountant.enter_journal('LOANS', 'SAVINGS_BANK', 100, datetime(2022, 8, 10), 'To a')
+        accountant.enter_journal('LOANS', 'SAVINGS_BANK', 200, datetime(2022, 8, 10), 'To b')
+        aging = get_accounts_aging(
+            account_config, accountant.journal.entries, ['SAVINGS_BANK', 'LOANS'], datetime(2022, 8, 10)
+        )
+
+        saving_bank_aging = aging['SAVINGS_BANK']
+        self.assertEqual(len(saving_bank_aging.ages), 1)
+        self.assertEqual(saving_bank_aging.ages[0].counter.is_paid(), True)
+        self.assertEqual(saving_bank_aging.ages[0].counter.get_balance(), 0)
+        self.assertEqual(saving_bank_aging.excess_amount, 100)
+        self.assertEqual(saving_bank_aging.last_sl_no, 5)
+        self.assertEqual(saving_bank_aging.last_unpaid_age_idx, 1)
+
+        loans_aging = aging['LOANS']
+        self.assertEqual(len(loans_aging.ages), 2)
+        self.assertEqual(loans_aging.ages[0].counter.is_paid(), False)
+        self.assertEqual(loans_aging.ages[0].counter.get_balance(), 100)
+        self.assertEqual(loans_aging.ages[1].counter.is_paid(), False)
+        self.assertEqual(loans_aging.ages[1].counter.get_balance(), 200)
+        self.assertEqual(loans_aging.excess_amount, 0)
+        self.assertEqual(loans_aging.last_sl_no, 4)
+        self.assertEqual(loans_aging.last_unpaid_age_idx, 0)
+
+        accountant.enter_journal('LOANS_PAYBACK', 'LOANS', 100, datetime(2022, 8, 11), 'From a')
+        accountant.enter_journal('LOANS_PAYBACK', 'LOANS', 199, datetime(2022, 8, 12), 'From b')
+        aging = get_accounts_aging(
+            account_config, accountant.journal.entries, ['LOANS'],
+            datetime(2022, 8, 12), previous_aging=aging
+        )
+        loans_aging = aging['LOANS']
+        self.assertEqual(len(loans_aging.ages), 2)
+        self.assertEqual(loans_aging.ages[0].counter.is_paid(), True)
+        self.assertEqual(loans_aging.ages[0].counter.get_balance(), 0)
+        self.assertEqual(loans_aging.ages[1].counter.is_paid(), False)
+        self.assertEqual(loans_aging.ages[1].counter.get_balance(), 1)
+        self.assertEqual(loans_aging.excess_amount, 0)
+        self.assertEqual(loans_aging.last_sl_no, 9)
+        self.assertEqual(loans_aging.last_unpaid_age_idx, 1)
+
+        with self.assertRaises(ValueError) as e:
+            get_accounts_aging(
+                account_config, accountant.journal.entries, ['LOANS_PAYBACK'],
+                datetime(2022, 8, 12), previous_aging=aging
+            )
+        self.assertEqual(e.exception.__str__(), 'Invalid previous aging! accounts not matching')
