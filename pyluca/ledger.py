89baaa86413252e3c2dc6588adf663cf5cd561d3
Sameer Saxena <sameer.saxena@datasignsfintech.com>
2024-04-08 16:16:30 +0530

    Using AccountLedger in Ledger

--- pyluca/ledger.py
+++ pyluca/ledger.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, NamedTuple
+from typing import List, Optional, NamedTuple, Dict
 from datetime import datetime
 from collections import defaultdict
 import pandas as pd
@@ -45,7 +45,7 @@ class AccountLedger:
 
     def get_balance(self, as_of: Optional[datetime] = None) -> float:
         if as_of is None:
-            return self.__entries[-1].balance
+            return self.__entries[-1].balance if len(self.__entries) else 0
 
         balance = 0
         start, end = 0, len(self.__entries) - 1
@@ -66,44 +66,64 @@ class Ledger:
     def __init__(self, journal: Journal, config: dict):
         self.journal = journal
         self.config = config
+        self.ledgers: Dict[str, AccountLedger] = {
+            account: AccountLedger(
+                account_name=account_config.get('name', account),
+                balance_type=BalanceType[config['account_types'][account_config['type']]['balance_type']]
+            )
+            for account, account_config in config['accounts'].items()
+        }
+        for je in journal.entries:
+            self.ledgers[je.account].add_entry(
+                date=je.date,
+                dr_amount=je.dr_amount,
+                cr_amount=je.cr_amount,
+                narration=je.narration,
+                event_id=je.event_id
+            )
 
-    def get_account_dr(self, account: str, as_of: Optional[datetime] = None):
+    def enter_ledger(
+            self,
+            dr_account: str,
+            cr_account: str,
+            amount: float,
+            date: datetime,
+            narration: str,
+            event_id: Optional[str] = None
+    ):
+        self.ledgers[dr_account].add_entry(
+            date=date,
+            dr_amount=amount,
+            cr_amount=0,
+            narration=narration,
+            event_id=event_id
+        )
+        self.ledgers[cr_account].add_entry(
+            date=date,
+            dr_amount=0,
+            cr_amount=amount,
+            narration=narration,
+            event_id=event_id
+        )
+
+    def get_account_dr(self, account: str, as_of: Optional[datetime] = None) -> float:
+        assert self.config['accounts'][account]['type'] in self.config['account_types'], f'Invalid account {account}'
         if as_of is not None:
-            return sum([j.dr_amount for j in self.journal.entries if j.account == account and j.date <= as_of])
-        return sum([j.dr_amount for j in self.journal.entries if j.account == account])
+            return sum([entry.dr_amount for entry in self.ledgers[account].get_entries() if entry.date <= as_of])
+        return sum([entry.dr_amount for entry in self.ledgers[account].get_entries()])
 
-    def get_account_cr(self, account: str, as_of: Optional[datetime] = None):
+    def get_account_cr(self, account: str, as_of: Optional[datetime] = None) -> float:
+        assert self.config['accounts'][account]['type'] in self.config['account_types'], f'Invalid account {account}'
         if as_of is not None:
-            return sum([j.cr_amount for j in self.journal.entries if j.account == account and j.date <= as_of])
-        return sum([j.cr_amount for j in self.journal.entries if j.account == account])
+            return sum([entry.cr_amount for entry in self.ledgers[account].get_entries() if entry.date <= as_of])
+        return sum([entry.cr_amount for entry in self.ledgers[account].get_entries()])
 
-    def get_account_balance(self, account: str, as_of: Optional[datetime] = None):
-        assert self.config['accounts'][account]['type'] in self.config['account_types']
-        if as_of is not None:
-            if self.config['account_types'][self.config['accounts'][account]['type']]['balance_type'] == BalanceType.DEBIT.value:
-                return sum([je.dr_amount - je.cr_amount for je in self.journal.entries if je.account == account and je.date <= as_of])
-            return sum([je.cr_amount - je.dr_amount for je in self.journal.entries if je.account == account and je.date <= as_of])
+    def get_account_balance(self, account: str, as_of: Optional[datetime] = None) -> float:
+        assert self.config['accounts'][account]['type'] in self.config['account_types'], f'Invalid account {account}'
+        return self.ledgers[account].get_balance(as_of)
 
-        if self.config['account_types'][self.config['accounts'][account]['type']]['balance_type'] == BalanceType.DEBIT.value:
-            return sum([je.dr_amount - je.cr_amount for je in self.journal.entries if je.account == account])
-        return sum([je.cr_amount - je.dr_amount for je in self.journal.entries if je.account == account])
-
-    def get_balances(self, as_of: Optional[datetime] = None) -> dict:
-        accounts_balance = defaultdict(float)
-        for je in self.journal.entries:
-            if self.config['account_types'][self.config['accounts'][je.account]['type']]['balance_type'] == BalanceType.DEBIT.value:
-                if as_of is not None:
-                    if je.date <= as_of:
-                        accounts_balance[je.account] += (je.dr_amount - je.cr_amount)
-                else:
-                    accounts_balance[je.account] += (je.dr_amount - je.cr_amount)
-            else:
-                if as_of is not None:
-                    if je.date <= as_of:
-                        accounts_balance[je.account] += (je.cr_amount - je.dr_amount)
-                else:
-                    accounts_balance[je.account] += (je.cr_amount - je.dr_amount)
-        return accounts_balance
+    def get_balances(self, as_of: Optional[datetime] = None) -> Dict[str, float]:
+        return {account: ledger.get_balance(as_of) for account, ledger in self.ledgers.items()}
 
     def get_df(self) -> pd.DataFrame:
         ledger_df = pd.DataFrame([j.__dict__ for j in self.journal.entries])
@@ -123,10 +143,15 @@ class Ledger:
             df = self.add_account_balance(acct_name, df)
         return df
 
-    def get_account_type_balance(self, account_type: str, exclude_accounts: List[str] = None):
+    def get_account_type_balance(
+            self,
+            account_type: str,
+            exclude_accounts: List[str] = None,
+            as_of: Optional[datetime] = None
+    ):
         balance = 0
         exclude_accounts = [] if exclude_accounts is None else exclude_accounts
         for account_name, account in self.config['accounts'].items():
             if account['type'] == account_type and account_name not in exclude_accounts:
-                balance += self.get_account_balance(account_name)
+                balance += self.get_account_balance(account_name, as_of)
         return balance
